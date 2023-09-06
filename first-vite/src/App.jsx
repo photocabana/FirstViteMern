123c2fe9a14cab06c7e359a2b913d1cffb74577a
Rindy Fisk <117949294+photocabana@users.noreply.github.com>
2023-09-06 17:44:45 -0500

    Add files via upload

---
+++ first-vite/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello Dojo!</h1>
+          <h2>Things I need to do:</h2>
+          <ul>
+            <li>Learn React</li>
+            <li>Get Sleep - hahahahahaa</li>
+            <li>Eat More Whole Foods</li>
+            <li>Feed My 2 Dogs</li>
+            <li>Feed My 17 Chickie-Chicks</li>
+          </ul>
+        </div>
+    </>
+  )
+}
+
+export default App
